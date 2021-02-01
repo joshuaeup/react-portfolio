ea4753d0ad02ff26100376574d235678a4bc4f3b
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-01-31 20:26:39 -0500

    Added side nav

--- src/Containers/Portfolio/Home/Home.js
+++ src/Containers/Portfolio/Home/Home.js
@@ -3,16 +3,25 @@ import React from "react";
 import Profile from "../../../Components/Profile/Profile";
 import Projects from "../../../Components/Projects/Projects";
 import Skills from "../../../Components/Skills/Skills";
+import Contact from "../../../Components/Contact/Contact";
+import Navigation from "../../../Components/Navigation/Navigation";
 
 const Home = (props) => {
     return (
-        <>
-            <Profile />
-            <hr className="border-line" />
-            <Projects />
-            <hr className="border-line" />
-            <Skills />
-        </>
+        <div id="body-two-column-grid">
+            <aside>
+                <Navigation />
+            </aside>
+            <div id="Main">
+                <Profile />
+                <hr className="border-line" />
+                <Projects />
+                <hr className="border-line" />
+                <Skills />
+                <hr className="border-line" />
+                <Contact />
+            </div>
+        </div>
     );
 };
 
--- src/Containers/Portfolio/Portfolio.js
+++ src/Containers/Portfolio/Portfolio.js
@@ -4,16 +4,10 @@ import { Route } from "react-router-dom";
 // Containers
 import Home from "./Home/Home";
 
-// Components
-import Navigation from "../../Components/Navigation/Navigation";
-
 const Portfolio = () => {
     return (
         <>
-            <Navigation />
-            <main id="Main">
-                <Route to="/" render={() => <Home />} />
-            </main>
+            <Route to="/" render={() => <Home />} />
         </>
     );
 };
