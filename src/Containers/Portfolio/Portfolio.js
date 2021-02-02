992df1a4e233a35525e886f0c6f36e42513bbe70
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-02 12:29:39 -0500

    Added loader

--- src/Containers/Portfolio/Portfolio.js
+++ src/Containers/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import Navigation from "../../Components/Navigation/Navigation";
 
@@ -6,15 +6,32 @@ import Navigation from "../../Components/Navigation/Navigation";
 import Home from "./Home/Home";
 
 const Portfolio = () => {
+    const [loading, setLoading] = useState(true);
+
+    // Updates loading state to false after 6 seconds
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 2000);
+    });
     return (
         <>
-            <i id="open-toggle" className="fas fa-bars "></i>
-            <div id="body-two-column-grid">
-                <aside>
-                    <Navigation />
-                </aside>
-                <Route to="/" render={() => <Home />} />
-            </div>
+            {loading === false ? (
+                <>
+                    <i id="open-toggle" className="fas fa-bars "></i>
+                    <div id="body-two-column-grid">
+                        <aside>
+                            <Navigation />
+                        </aside>
+                        <Route to="/" render={() => <Home />} />
+                    </div>
+                </>
+            ) : (
+                <div id="loader-container">
+                    <div>
+                        <h1 id="loader__title">Loading...</h1>
+                        <div id="loader"></div>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
