b2c732188ac0534af9d47a5c7d30f3b8e520fa6a
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-03 10:33:50 -0500

    render projects dynamically

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,98 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../Title/Title";
+import ProjectsData from "./ProjectsData";
 
 const Projects = (props) => {
+    const [projects] = useState(ProjectsData);
     return (
         <>
             <div id="projects-container">
                 <hr className="border-line" />
                 <Title text="Projects" />
                 <div className="portfolio-column-grid">
-                    <div className="portfolio-column-grid__column">
-                        <div className="portfolio-column-grid__column__img__container">
-                            <img
-                                className="portfolio-column-grid__column__img"
-                                alt="Portfolio Item"
-                                src="https://joshuaeup.github.io/my-port/static/media/gateWayBanner.70a98591.PNG"
-                            />
-                        </div>
-                        <div className="portfolio-column-grid__column__info">
-                            <h2 className="portfolio-column-grid__column__info__title">
-                                Portfolio Item
-                            </h2>
-                            <p className="portfolio-column-grid__column__info__text">
-                                Personal (Web)
-                            </p>
-                        </div>
-                    </div>
-                    <div className="portfolio-column-grid__column">
-                        <div className="portfolio-column-grid__column__img__container">
-                            <img
-                                className="portfolio-column-grid__column__img"
-                                alt="Portfolio Item"
-                                src="https://joshuaeup.github.io/my-port/static/media/gateWayBanner.70a98591.PNG"
-                            />
-                        </div>
-                        <div className="portfolio-column-grid__column__info">
-                            <h2 className="portfolio-column-grid__column__info__title">
-                                Portfolio Item
-                            </h2>
-                            <p className="portfolio-column-grid__column__info__text">
-                                Personal (Web)
-                            </p>
-                        </div>
-                    </div>
-                    <div className="portfolio-column-grid__column">
-                        <div className="portfolio-column-grid__column__img__container">
-                            <img
-                                className="portfolio-column-grid__column__img"
-                                alt="Portfolio Item"
-                                src="https://joshuaeup.github.io/my-port/static/media/gateWayBanner.70a98591.PNG"
-                            />
-                        </div>
-                        <div className="portfolio-column-grid__column__info">
-                            <h2 className="portfolio-column-grid__column__info__title">
-                                Portfolio Item
-                            </h2>
-                            <p className="portfolio-column-grid__column__info__text">
-                                Personal (Web)
-                            </p>
-                        </div>
-                    </div>
-                    <div className="portfolio-column-grid__column">
-                        <div className="portfolio-column-grid__column__img__container">
-                            <img
-                                className="portfolio-column-grid__column__img"
-                                alt="Portfolio Item"
-                                src="https://joshuaeup.github.io/my-port/static/media/gateWayBanner.70a98591.PNG"
-                            />
-                        </div>
-                        <div className="portfolio-column-grid__column__info">
-                            <h2 className="portfolio-column-grid__column__info__title">
-                                Portfolio Item
-                            </h2>
-                            <p className="portfolio-column-grid__column__info__text">
-                                Personal (Web)
-                            </p>
-                        </div>
-                    </div>
-                    <div className="portfolio-column-grid__column">
-                        <div className="portfolio-column-grid__column__img__container">
-                            <img
-                                className="portfolio-column-grid__column__img"
-                                alt="Portfolio Item"
-                                src="https://joshuaeup.github.io/my-port/static/media/gateWayBanner.70a98591.PNG"
-                            />
-                        </div>
-                        <div className="portfolio-column-grid__column__info">
-                            <h2 className="portfolio-column-grid__column__info__title">
-                                Portfolio Item
-                            </h2>
-                            <p className="portfolio-column-grid__column__info__text">
-                                Personal (Web)
-                            </p>
-                        </div>
-                    </div>
+                    {projects.map((project, id) => {
+                        return (
+                            <div
+                                className="portfolio-column-grid__column"
+                                key={id}
+                            >
+                                <div className="portfolio-column-grid__column__img__container">
+                                    <img
+                                        className="portfolio-column-grid__column__img"
+                                        alt={project.title}
+                                        src={project.image}
+                                    />
+                                </div>
+                                <div className="portfolio-column-grid__column__info">
+                                    <h2 className="portfolio-column-grid__column__info__title">
+                                        {project.title}
+                                    </h2>
+                                    <p className="portfolio-column-grid__column__info__text">
+                                        {project.subTitle}
+                                    </p>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </>
